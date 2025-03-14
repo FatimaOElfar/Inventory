497a89104ee4ee451a3934532f0c26b0cd9aa529
FatimaOElfar <fatma.2002nagy@gmail.com>
2025-03-15 01:29:58 +0200

    Merged fatma into main and resolved conflicts

--- obj/Debug/net9.0-windows/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/InventoryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58403cf294542bc0934b2e4ab3f25ca0b6828d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342b47981db5539cdab04304303e736c309cb713")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
