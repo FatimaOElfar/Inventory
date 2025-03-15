743a30635f3a7b0586ec9df8442d4fda4bbd8e3b
FatimaOElfar <fatma.2002nagy@gmail.com>
2025-03-15 02:34:02 +0200

    upload

--- obj/Debug/net9.0-windows/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/InventoryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7522679d0653cdd9090a0f4aa57c14f62616efeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a44adeedb674dcff3b16c17b9718fe34b0808b29")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
